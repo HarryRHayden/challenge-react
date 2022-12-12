582e6cacfd297365fdf37aee872ff5f70f2c1794
Harry Hayden <h_man1992@hotmail.com>
2022-12-12 11:59:48 +0000

    add login/logout

---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,42 @@
+import React from "react";
+import css from "./css/NavBarSimple.module.css";
+
+class NavBarSimple extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "Hello, guest!",
+            name: "log in",
+        };
+    }
+
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+            console.log("Previous State", prevState)
+            console.log("Previous Props", prevProps)
+            return {
+                message: prevState.message === "Hello, guest!" ? "Welcome back, user!" : "Hello, guest!",
+                name: prevState.name === "log in" ? "log out" : "log in"
+            }
+        })
+    }
+    
+    render() {
+        return (
+            <div class={css.NavBar}>
+                <h1>
+                    My Gallery
+                </h1>
+                <span>
+                    {this.state.message}
+                </span>
+                <button onClick={() => this.handleClick()}>
+                    {this.state.name}
+                </button>
+            </div>
+        )
+    }
+}
+
+export default NavBarSimple;
